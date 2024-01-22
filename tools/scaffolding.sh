1e758c7dbc1165cfd831cbd13603cf05b2ccc8df
khaifahmi99 <khaifahmi99@github.com>
2024-01-22 22:15:11 +1100

    chore: add a scaffolding script
    Takes in a name as a parameter and it will create the file structure as well as an empty notebook

---
+++ tools/scaffolding.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+folder_name=$1
+folder_name=$(echo "$folder_name" | sed 's/\/$//')
+
+# early exit if the folder already exists
+if [ -d "$folder_name" ]; then
+  echo "Folder $folder_name already exists"
+  exit 1
+fi
+
+mkdir -p "$folder_name/input" 
+mkdir -p "$folder_name/output"
+mkdir -p "$folder_name/docs/images"
+
+echo '{ "cells": [], "metadata": {}, "nbformat": 4, "nbformat_minor": 2 }' > "$folder_name/main.ipynb"
+
+GENERATED_CONTENT="# Auto-generated from directory scaffolding"
+
+echo -e "$GENERATED_CONTENT" > "$folder_name/input/dir.txt"
+echo -e "## This directory is for input files" >> "$folder_name/input/dir.txt"
+
+echo -e "$GENERATED_CONTENT" > "$folder_name/output/dir.txt"
+echo -e "## This directory is for output files" >> "$folder_name/output/dir.txt"
+
+echo -e "$GENERATED_CONTENT" > "$folder_name/docs/images/dir.txt"
+echo -e "## This directory is for images needed for the docs" >> "$folder_name/input/dir.txt" 
+
+echo -e "Scaffolding complete! \n\nYou can now edit your new project in the $folder_name folder."
